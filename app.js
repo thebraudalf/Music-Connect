0b70bb98994feab797fd7664b323b95ee20bdbff
Nehal Jain <niihaljain2005@gmail.com>
2025-04-21 22:19:21 +0530

    add socket server for real time communication

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import {Server} from "socket.io";
+import { createServer } from "http";
+import { initializeSocketIO } from "./utils/socket/index.js";
 
 const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    pingTimeout: 60000,
+    cors: {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    },
+});
+
+app.set("io", io); // using set method to mount the `io` instance on the app to avoid usage of `global
+
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -16,7 +32,12 @@ app.use(cookieParser());
 
 // import routes
 import userRouter from "./routes/user.routes.js";
+import messageRouter from "./routes/message.routes.js";
+
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/messages", messageRouter);
 
-export { app }
+initializeSocketIO(io); // initialize the socket io
+
+export { httpServer }

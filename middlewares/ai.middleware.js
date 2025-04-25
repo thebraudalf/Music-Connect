8b5bfea2cabadbb8c306b22e778a9e20841d0add
Nehal Jain <niihaljain2005@gmail.com>
2025-04-25 13:11:09 +0530

    add changes related similarity search functionality

--- middlewares/ai.middleware.js
+++ middlewares/ai.middleware.js
@@ -1,9 +1,10 @@
 import { generateChatCompetion } from "../utils/ai/PromptTemplate.js";
 import { asyncHandler } from "../utils/AsyncHandler.js";
-// import { Response } from "../models/response.model.js";
-//import { getEmbedding } from "../utils/ai/generateEmbeddings.js";
+import { Response } from "../models/response.model.js";
+import { getEmbedding } from "../utils/ai/generateEmbeddings.js";
 import { ApiError } from "../utils/ApiError.js";
-// import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import e from "cors";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 const chatGeneration = asyncHandler(async (req, res, next) => {
     // Step 1: Extract the user message from the request body
@@ -18,43 +19,90 @@ const chatGeneration = asyncHandler(async (req, res, next) => {
         const { receiverMessage } = req.body;
         //console.log("receiverMessage", receiverMessage);
 
+        // checking if the receiverMessage and content is present in the request body
+        if (!receiverMessage) {
+            throw new ApiError(400, "Receiver message are required");
+        }
+
         // calling the generateChatCompetion func with the user message
-        // const chatQuestionResponse = await generateChatCompetion(receiverMessage);
+        const chatQuestionResponse = await generateChatCompetion(receiverMessage);
         //console.log("chatResponse", chatQuestionResponse);
 
-        // if (!chatQuestionResponse) {
-            // throw new ApiError(400, "Unabe to generate chat question response");
-        // }
+        if (!chatQuestionResponse) {
+            throw new ApiError(400, "Unabe to generate chat question response");
+        }
+
+        // Chunk the song lyrics into smaller parts
+        const textsplitters = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 20,
+        });
+        const chunks = await textsplitters.splitText(chatQuestionResponse); // Split the texts into chunks
+        //console.log("Chunks: ", chunks);
 
 
         // Generating embeddings for the generated user message
-        // const embeddings = await getEmbedding("sad");
-        // //console.log("embeddings", embeddings);
-
-        // if (!embeddings) {
-        //     throw new ApiError(400, "Unable to generate embeddings");
+        const embeddings = await getEmbedding(chunks);
+        //console.log("embeddings", embeddings);
+        // if (embeddings && Array.isArray(embeddings) && embeddings.length === 768) {
+        //     // If the embedding is valid, push it to the embeddings array
+        //     if (!embeddings) {
+        //         embeddings = embedding;
+        //     } else {
+        //         embeddings.push(embedding);
+        //     }
+        // } else {
+        //     console.error("Invalid embedding for chunk:", chunk);
         // }
+
+        // const embeddings = await getEmbedding(chunks);
+        // console.log("embeddings", embeddings);
+        // const embeddings = [];
+        // for (const chunk of chunks) {
+        //     const embedding = await getEmbedding(chunk); // embedding for just 1 chunk
+
+        //     if (embedding && Array.isArray(embedding) && embedding.length === 768) {
+        //         // If the embedding is valid, push it to the embeddings array
+        //         if (!embeddings) {
+        //             embeddings = embedding;
+        //         } else {
+        //             embeddings.push(embedding);
+        //         }
+        //     } else {
+        //         console.error("Invalid embedding for chunk:", chunk);
+        //     }
+        // }
+        //console.log("embeddings", embeddings);
+
+
+        if (!embeddings) {
+            throw new ApiError(400, "Unable to generate embeddings");
+        }
 
 
         // Performing vector search through response model with generated chat response
-        /*const response = await Response.aggregate([
+        const results = await Response.aggregate([
             {
                 $vectorSearch: {
-                    index: "vector_indexes",
-                    queryVector: embeddings,
+                    index: "vector_indexes", // This index must be of type `vector`
                     path: "embeddings",
-                    exact: true,
-                    limit: 5
+                    queryVector: embeddings,
+                    numCandidates: 100,
+                    Dimensions: 768,
+                    limit: 5,
+                    similarity: "cosine"
                 }
+
             },
             {
                 $project: {
-                   musicData: 1,
+                    // _id: 0,
+                    musicData: 1,
+                    // score: { $meta: "vectorSearchScore" }
                 }
             }
-        ]); */
-        //console.log("response", response);
-        
+        ]);
+        //console.log("response", results);
 
         // if (response.length === 0) {
         //     throw new ApiError(400, "Unabe to perform vector search");
@@ -62,7 +110,7 @@ const chatGeneration = asyncHandler(async (req, res, next) => {
 
 
         // Extracting the vector search result and again call the generateChatCompetion func and pass the vector search result as a message to the function
-        const chatResponse = await generateChatCompetion(undefined, receiverMessage);
+        const chatResponse = await generateChatCompetion(undefined, results);
         //console.log("chatResponse", chatResponse);
 
         if (!chatResponse) {

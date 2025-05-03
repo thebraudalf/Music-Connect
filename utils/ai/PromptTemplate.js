7af49a1116aa5a1948faea88c50330690fdba67d
Nehal Jain <niihaljain2005@gmail.com>
2025-05-04 00:24:13 +0530

    commend dotenv

--- utils/ai/PromptTemplate.js
+++ utils/ai/PromptTemplate.js
@@ -1,8 +1,5 @@
 import { Groq } from 'groq-sdk';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
+//import dotenv from 'dotenv';
 
 const groq = new Groq({
     apiKey: process.env.GROQ_API_KEY,
